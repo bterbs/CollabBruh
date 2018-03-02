dfaf48605aeff5a88929a7a81481b9c7ab20bac2
B Terbush <brittanyterbush@gmail.com>
2018-03-01 16:51:27 -0800

    Restructure for tests/ test config files

---
+++ src/components/app.test.js
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment node
+ */
+ import React from 'react';
+ import { shallow } from 'enzyme';
+
+ import App from './App';
+
+ const app = shallow(<App />);
+
+ it('renders correctly', () => {
+   expect(app).toMatchSnapshot()
+ })
