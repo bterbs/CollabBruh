cc4e3710358d951cd5594406d91189787fbed325
B Terbush <brittanyterbush@gmail.com>
2018-03-03 19:55:05 -0800

    WIP: fix rendering issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div>You ready to Collab? </div>
     )
   }
 }
-
-//tell React to render HTML in the DOM
-ReactDOM.render(<App />, document.querySelector('.container'))
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App'
+
+//tell React to render HTML in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'))
