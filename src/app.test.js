6175cc6caecad537555e529ef62c729af0fe858d
B Terbush <brittanyterbush@gmail.com>
2018-03-03 20:46:07 -0800

    Add jest config to package.json under beneath scripts. Point to setupTests file. Test suite now works git push

--- src/app.test.js
+++ src/app.test.js
@@ -5,7 +5,7 @@
  import React from 'react';
  import { shallow } from 'enzyme';
 
- import App from './App';
+ import App from './components/App';
 
  const app = shallow(<App />);
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Switch } from 'react-router-dom'
 
 import App from './components/App'
 
 //tell React to render HTML in the DOM
-ReactDOM.render(<App />, document.querySelector('.container'))
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>
+  , document.querySelector('.container'))
